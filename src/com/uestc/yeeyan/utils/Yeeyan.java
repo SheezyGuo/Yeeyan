90ae908fea9e6596bb4112857d1c837ad2a57f06
郭世豪 <dtlvhyy@gmail.com>
2015-03-16 10:01:04 +0800

    完成MD5部分

--- src/com/uestc/yeeyan/utils/Yeeyan.java
+++ src/com/uestc/yeeyan/utils/Yeeyan.java
@@ -20,7 +20,6 @@ import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.htmlparser.Node;
 import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
 import org.htmlparser.filters.HasAttributeFilter;
@@ -30,7 +29,7 @@ import org.htmlparser.util.ParserException;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.Mongo;
+import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 
 public class Yeeyan {
@@ -167,7 +166,6 @@ public class Yeeyan {
 			URL url = new URL(imageUrl);
 			File outFile = new File(storeDir + File.separator
 					+ imageRelativePath);
-			@SuppressWarnings("resource")
 			OutputStream os = new FileOutputStream(outFile);
 			InputStream is = url.openStream();
 			byte[] buffer = new byte[10240];
@@ -198,7 +196,7 @@ public class Yeeyan {
 		String title = getFiltContent(parser, new String[] { "class",
 				"sa_title sa_title_pr" });
 		System.out.println("title:"+title);
-		String content = getFiltContent(parser, new String[] { "class","sa_content" });
+		String content = getFiltContent(parser, new String[] { "class","sa_content" }).replaceAll("\\n\\s*","\n");
 		System.out.println("Content:"+content);
 // 用正则表达式获取时间 当通过sa_author获取不准确时可用		
 //		String timeReg = "(?<=<span>发布：)\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}(?=</span>)";
@@ -214,7 +212,7 @@ public class Yeeyan {
 //		}
 		String rawDate = getFiltContent(parser, new String[] { "class","sa_author" });
 		String date = rawDate.substring(rawDate.indexOf("发布：")+"发布：".length(),rawDate.lastIndexOf("挑错"));
-		String MD5 = getMD5(htmlCode);
+		String MD5 = getMD5(content);
 		// <img
 		// src="http://static.yeeyan.org/upload/image/2015/03/13/14262218586.jpg"
 		// />
@@ -228,7 +226,7 @@ public class Yeeyan {
 			System.out.println("imageUrl:"+imageUrl);
 			String postfix = imageUrl.substring(imageUrl.lastIndexOf(".")+1,
 					imageUrl.length()).replace(" ", "");
-			String relativePath = String.format("%s%04d.%s", MD5, imageNum,
+			String relativePath = String.format("%s_%04d.%s", MD5, imageNum,
 					postfix);
 			downloadImage(imageUrl, this.storeDir, relativePath);
 			String imagePath = String.format("%s%s%s", storeDir,
@@ -243,7 +241,15 @@ public class Yeeyan {
 				.append("Content", content).append("Date", date)
 				.append("ImagePath", imagePathQueue.toString())
 				.append("URL", targetUrl).append("MD5", MD5);
-		coll.insert(doc);
+		BasicDBObject query = new BasicDBObject("MD5",MD5);
+		DBCursor cursor = coll.find(query); 
+		//如果记录中没用相同MD5值的文章则添加记录
+		if(!cursor.hasNext()){
+			coll.insert(doc);
+		}
+		else{
+			//System.out.println("Find same record");
+		}
 		mongoClient.close();
 
 	}
@@ -266,17 +272,18 @@ public class Yeeyan {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Yeeyan yeeyan = new Yeeyan();
-		//yeeyan.storeContent("http://select.yeeyan.org/view/305107/446753");
-		String[] strs = new String[]{
-				"密算法，后因MD5安全性更高，DES被淘汰）、通信信",
-				"息加密（如大家熟悉的即时通信软件MyIM）、数字签名等诸多方面。",
-				"MD5将任意长度的“字节串”变换成一个128bit的大整数，并且它是一个不可逆的字,",
-				"符串变换算法，换句话说就是，即使你看到源程序和算法描述，也无法将一个MD5的值变换回原始的字符串，从数学原理上说，是因为原始的字符串有无穷多个，这有点象不存在反函数的数学函数。",
-				"MD5的典型应用是对一段Message(字节串)产生fingerprint(指纹)，以防止被“篡改”。举个例子，你将一段话写在一个叫 readme.txt文件中，",
-				"并对这个readme.txt产生一个MD5的值并记录在案，然后你可以传播这个文件给别"
-		};
-		for(String str:strs){
-			System.out.println(yeeyan.getMD5(str));
-		}
+		yeeyan.storeContent("http://select.yeeyan.org/view/305107/446753");
+		
+//		String[] strs = new String[]{
+//				"\n\t\t\t\t\n\t作者：Gerd\nLeonhard \n\n\n\t \n\n超级互联的未来既美好又可怕，正如新加坡的这些高科技“超级树”。\n\n\t智能手机和移动设备给了所有人第二个大脑，为身处陌生城市中的我们进行导航，不致迷路，同时，帮助我们用多种语言进行顺畅的交流。\n\n\n\t这些设备使得我们的生活越来越便利，很快就将从衣服口袋转移至体内——试想一下虹膜植入和大脑植入，赋予我们前所未有的互联性。\n\n\n\t显然，移动互联的乐趣和便利不胜枚举，令人无法抗拒。若想置之不理，无疑是徒劳之举。\n\n\n\t不过在不久的将来，我料想一系列意料之外的扰人后果将会显现，超级互联的美好未来将渐渐化为泡影。\n\n\n\t我们在Flickr上分享自己孩子照片的时候，也许不会想到它们会出现在Koppie-Koppie网站正在出售的咖啡杯上。\n\n\n\t在推特上评论政治应该不至于让人身陷囹圄，不过这样的事目前正在土耳其上演。\n\n\n\t为脸书的某家公司点赞不应沦为人人都能参与的市场营销。\n\n\n\t拥有一部带有SIM卡的手机不应沦为政府安全部门监听公众电话的永恒捷径。\n\n\n\t这类事件层出不穷，以上只是冰山一角。互联技术的滥用趋势如火如荼。\n\n\n\t2020年，预计有超过50亿人、1000亿“事物”将连接至互联网——其中相当一部分通过无线、移动设备等手段进行连接。\n\n\n\t人工智能等科技的飞速发展和无处不在的互联技术正逐步向核能靠拢。核能技术前景辽阔，颇有益处，不过实际应用后，一旦出现什么差池，我们压根不清楚如何加以制止。 \n\n\n\t愈加显而易见的一点是，若没有强制性的全球标准明确什么该允许，什么又该禁止，日后我们将频繁遭遇灾难，或者沦为另一个福岛[1]。\n\n\n\t若没有相应法规的出台，灾难性事件一触即发。我们需要制定标准和规则来疏导这股强大的力量。\n\n\n\t消费者需要强大的科技，让生活更便利、更丰富、更高效。不过，假如科技意味着整个社会逐渐向“机器人化”的方向发展，意味着堕落与沉迷，意味着个人隐私的丧失和无端的监控，那么他们是不会使用这些移动设备和网络的。\n\n\n\t确实，我们中的大多数人目前仍在享受全新移动技术，超级便利的手机软件、地图以及智能数字助手所具备的“超能力”。这些技术和设备如此新鲜、富有个性，以致很多人根本不在乎自己在虚拟世界变得毫无隐私可言。\n\n\n\t不过可以肯定的是，我们中的许多“数字原住民”很快将奋起反抗，拒绝再居住在这样一个庞大的“大数据”泡沫里。身处其中，我们不再是科技的主人或者说受益人，只会沦为无足轻重的人，沦为过度营销和远程监控的受害者。\n\n\n\t整个游戏已经发生变化，移动行业最好注意到这一点。建议行业领导者们目光放长远一些。\n\n\n\t问题的关键不再是如何实现人与人的互联，而是一旦实现这一目标，将会发生什么。\n\n\n\t如果疲软的数据保护标准、过时的社会契约以及摇摇欲坠的道德底线成为一种社会风气，那么极少有消费者会对数字货币、可穿戴设备和移动健康服务激动不已。\n\n\n\t是时候将加密技术视为一种默认的公共电子数据标准和人人都尊重的普遍权利了。\n\n\n\t\n\n\n\t译注：\n\n\n\t[1]福岛第一核电站在2011年“3·11”大地震和海啸中遭受重创，引发严重的辐射泄漏事故。\n\n\n\t\n\n\n\t\n\t\t————————————————————————————————————————\n\t\n\t\n\t\t*未经本人许可，请勿转载！\n\t\n\n\n\t———————————————————————————————————————— \n\n\n\t* 本译文来自译言「近未来」小组，是译言的科技主题翻译项目,欢迎加入。组内的优秀译文（以编译为主）将被收录于腾讯出品的思想内刊《腾云》（采用有稿酬）。译文未经授权请勿转载，合作及授权请联系：editor@yeeyan.com \n\n\n\t\t\t"				
+//		};
+//		for(String str:strs){
+//			System.out.println(yeeyan.getMD5(str));
+//		}
+//		String code = yeeyan.getHtmlCode("http://select.yeeyan.org/view/305107/446753");
+//		Parser parser = Parser.createParser(code, "utf-8");
+//		String content = yeeyan.getFiltContent(parser, new String[]{"class","sa_content"});
+//		System.out.println(content.replaceAll("\\n\\s*","\n"));
 	}
 }
+//74f14602b368d3f530ef760f6d96f122
